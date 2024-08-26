e928fa327d229232ba5ea163214ac1801123380f
Piano Nekomiya <kotone.olin1010@gmail.com>
2024-08-26 20:01:20 +0900

    unsafeな部分を修正

--- src/routes/api/db/new/+server.ts
+++ src/routes/api/db/new/+server.ts
@@ -7,6 +7,7 @@ export const POST: RequestHandler = async ({platform, request}) => {
     if (new Date() <= conf.limit) {
         let data: ContentDBTable = await request.json();
         data.id = data.id ? data.id : null;
+        data.approved = false;
         let time = data.time.split(/-|T|\s|:|\+/);
         if (platform?.env.DISCORD_WEBHOOK_URL) {
             await fetch(platform?.env.DISCORD_WEBHOOK_URL, {
